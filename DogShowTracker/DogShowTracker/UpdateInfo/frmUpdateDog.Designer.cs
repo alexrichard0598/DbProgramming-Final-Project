203798adcc352092148cae9d5de6966107a995fc
Alex Richard <alex.richard0598@pm.me>
2020-06-04 21:11:16 -0300

    Designed Update Dog Form

--- DogShowTracker/DogShowTracker/UpdateInfo/frmUpdateDog.Designer.cs
+++ DogShowTracker/DogShowTracker/UpdateInfo/frmUpdateDog.Designer.cs
@@ -28,12 +28,378 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.grpDogInfo = new System.Windows.Forms.GroupBox();
+            this.label21 = new System.Windows.Forms.Label();
+            this.dtpChampionshipDate = new System.Windows.Forms.DateTimePicker();
+            this.label20 = new System.Windows.Forms.Label();
+            this.dtpDateBanned = new System.Windows.Forms.DateTimePicker();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.chkChampion = new System.Windows.Forms.CheckBox();
+            this.chkBanned = new System.Windows.Forms.CheckBox();
+            this.label17 = new System.Windows.Forms.Label();
+            this.chkRetired = new System.Windows.Forms.CheckBox();
+            this.cmbOwner = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.cmbBreed = new System.Windows.Forms.ComboBox();
+            this.dtpDateOfRetirement = new System.Windows.Forms.DateTimePicker();
+            this.dtpDateOfBirth = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.grpSex = new System.Windows.Forms.GroupBox();
+            this.rdoFemale = new System.Windows.Forms.RadioButton();
+            this.rdoMale = new System.Windows.Forms.RadioButton();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.grpDogInfo.SuspendLayout();
+            this.grpSex.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpDogInfo
+            // 
+            this.grpDogInfo.Controls.Add(this.label21);
+            this.grpDogInfo.Controls.Add(this.dtpChampionshipDate);
+            this.grpDogInfo.Controls.Add(this.label20);
+            this.grpDogInfo.Controls.Add(this.dtpDateBanned);
+            this.grpDogInfo.Controls.Add(this.txtName);
+            this.grpDogInfo.Controls.Add(this.chkChampion);
+            this.grpDogInfo.Controls.Add(this.chkBanned);
+            this.grpDogInfo.Controls.Add(this.label17);
+            this.grpDogInfo.Controls.Add(this.chkRetired);
+            this.grpDogInfo.Controls.Add(this.cmbOwner);
+            this.grpDogInfo.Controls.Add(this.label9);
+            this.grpDogInfo.Controls.Add(this.cmbBreed);
+            this.grpDogInfo.Controls.Add(this.dtpDateOfRetirement);
+            this.grpDogInfo.Controls.Add(this.dtpDateOfBirth);
+            this.grpDogInfo.Controls.Add(this.label1);
+            this.grpDogInfo.Controls.Add(this.label2);
+            this.grpDogInfo.Controls.Add(this.label4);
+            this.grpDogInfo.Controls.Add(this.txtHeight);
+            this.grpDogInfo.Controls.Add(this.label5);
+            this.grpDogInfo.Controls.Add(this.txtWeight);
+            this.grpDogInfo.Controls.Add(this.label6);
+            this.grpDogInfo.Controls.Add(this.txtID);
+            this.grpDogInfo.Controls.Add(this.grpSex);
+            this.grpDogInfo.Controls.Add(this.label8);
+            this.grpDogInfo.Location = new System.Drawing.Point(12, 12);
+            this.grpDogInfo.Name = "grpDogInfo";
+            this.grpDogInfo.Size = new System.Drawing.Size(477, 289);
+            this.grpDogInfo.TabIndex = 17;
+            this.grpDogInfo.TabStop = false;
+            this.grpDogInfo.Text = "Dog Information";
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.Location = new System.Drawing.Point(227, 113);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(102, 13);
+            this.label21.TabIndex = 31;
+            this.label21.Text = "Championship Date:";
+            // 
+            // dtpChampionshipDate
+            // 
+            this.dtpChampionshipDate.Checked = false;
+            this.dtpChampionshipDate.CustomFormat = " ";
+            this.dtpChampionshipDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpChampionshipDate.Location = new System.Drawing.Point(335, 107);
+            this.dtpChampionshipDate.Name = "dtpChampionshipDate";
+            this.dtpChampionshipDate.Size = new System.Drawing.Size(132, 20);
+            this.dtpChampionshipDate.TabIndex = 30;
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Location = new System.Drawing.Point(256, 188);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(73, 13);
+            this.label20.TabIndex = 29;
+            this.label20.Text = "Date Banned:";
+            // 
+            // dtpDateBanned
+            // 
+            this.dtpDateBanned.Checked = false;
+            this.dtpDateBanned.CustomFormat = " ";
+            this.dtpDateBanned.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpDateBanned.Location = new System.Drawing.Point(335, 182);
+            this.dtpDateBanned.Name = "dtpDateBanned";
+            this.dtpDateBanned.Size = new System.Drawing.Size(132, 20);
+            this.dtpDateBanned.TabIndex = 28;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(81, 51);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(132, 20);
+            this.txtName.TabIndex = 27;
+            // 
+            // chkChampion
+            // 
+            this.chkChampion.AutoSize = true;
+            this.chkChampion.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.chkChampion.Location = new System.Drawing.Point(274, 84);
+            this.chkChampion.Name = "chkChampion";
+            this.chkChampion.Size = new System.Drawing.Size(76, 17);
+            this.chkChampion.TabIndex = 26;
+            this.chkChampion.Text = "Champion:";
+            this.chkChampion.UseVisualStyleBackColor = true;
+            // 
+            // chkBanned
+            // 
+            this.chkBanned.AutoSize = true;
+            this.chkBanned.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.chkBanned.Location = new System.Drawing.Point(284, 159);
+            this.chkBanned.Name = "chkBanned";
+            this.chkBanned.Size = new System.Drawing.Size(66, 17);
+            this.chkBanned.TabIndex = 25;
+            this.chkBanned.Text = "Banned:";
+            this.chkBanned.UseVisualStyleBackColor = true;
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(242, 54);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(87, 13);
+            this.label17.TabIndex = 22;
+            this.label17.Text = "Retirement Date:";
+            // 
+            // chkRetired
+            // 
+            this.chkRetired.AutoSize = true;
+            this.chkRetired.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.chkRetired.Location = new System.Drawing.Point(287, 25);
+            this.chkRetired.Name = "chkRetired";
+            this.chkRetired.Size = new System.Drawing.Size(63, 17);
+            this.chkRetired.TabIndex = 21;
+            this.chkRetired.Text = "Retired:";
+            this.chkRetired.UseVisualStyleBackColor = true;
+            // 
+            // cmbOwner
+            // 
+            this.cmbOwner.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbOwner.FormattingEnabled = true;
+            this.cmbOwner.Location = new System.Drawing.Point(335, 252);
+            this.cmbOwner.Name = "cmbOwner";
+            this.cmbOwner.Size = new System.Drawing.Size(132, 21);
+            this.cmbOwner.TabIndex = 20;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(251, 255);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(78, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Current Owner:";
+            // 
+            // cmbBreed
+            // 
+            this.cmbBreed.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbBreed.FormattingEnabled = true;
+            this.cmbBreed.Location = new System.Drawing.Point(335, 221);
+            this.cmbBreed.Name = "cmbBreed";
+            this.cmbBreed.Size = new System.Drawing.Size(132, 21);
+            this.cmbBreed.TabIndex = 18;
+            // 
+            // dtpDateOfRetirement
+            // 
+            this.dtpDateOfRetirement.Checked = false;
+            this.dtpDateOfRetirement.CustomFormat = " ";
+            this.dtpDateOfRetirement.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpDateOfRetirement.Location = new System.Drawing.Point(335, 48);
+            this.dtpDateOfRetirement.Name = "dtpDateOfRetirement";
+            this.dtpDateOfRetirement.Size = new System.Drawing.Size(132, 20);
+            this.dtpDateOfRetirement.TabIndex = 17;
+            // 
+            // dtpDateOfBirth
+            // 
+            this.dtpDateOfBirth.Location = new System.Drawing.Point(81, 248);
+            this.dtpDateOfBirth.Name = "dtpDateOfBirth";
+            this.dtpDateOfBirth.Size = new System.Drawing.Size(132, 20);
+            this.dtpDateOfBirth.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Weight:";
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(81, 202);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(132, 20);
+            this.txtHeight.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 205);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Height:";
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.Location = new System.Drawing.Point(81, 159);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(132, 20);
+            this.txtWeight.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 255);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Date of Birth:";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(81, 19);
+            this.txtID.Name = "txtID";
+            this.txtID.ReadOnly = true;
+            this.txtID.Size = new System.Drawing.Size(132, 20);
+            this.txtID.TabIndex = 10;
+            // 
+            // grpSex
+            // 
+            this.grpSex.Controls.Add(this.rdoFemale);
+            this.grpSex.Controls.Add(this.rdoMale);
+            this.grpSex.Location = new System.Drawing.Point(81, 90);
+            this.grpSex.Name = "grpSex";
+            this.grpSex.Size = new System.Drawing.Size(132, 50);
+            this.grpSex.TabIndex = 9;
+            this.grpSex.TabStop = false;
+            this.grpSex.Text = "Sex";
+            // 
+            // rdoFemale
+            // 
+            this.rdoFemale.AutoSize = true;
+            this.rdoFemale.Location = new System.Drawing.Point(67, 19);
+            this.rdoFemale.Name = "rdoFemale";
+            this.rdoFemale.Size = new System.Drawing.Size(59, 17);
+            this.rdoFemale.TabIndex = 1;
+            this.rdoFemale.TabStop = true;
+            this.rdoFemale.Text = "Female";
+            this.rdoFemale.UseVisualStyleBackColor = true;
+            // 
+            // rdoMale
+            // 
+            this.rdoMale.AutoSize = true;
+            this.rdoMale.Location = new System.Drawing.Point(6, 19);
+            this.rdoMale.Name = "rdoMale";
+            this.rdoMale.Size = new System.Drawing.Size(48, 17);
+            this.rdoMale.TabIndex = 0;
+            this.rdoMale.TabStop = true;
+            this.rdoMale.Text = "Male";
+            this.rdoMale.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(291, 224);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Breed:";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(306, 307);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 18;
+            this.btnUpdate.Text = "Update Dog";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(404, 307);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 19;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // frmUpdateDog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmUpdateDog";
+            this.ClientSize = new System.Drawing.Size(503, 340);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.grpDogInfo);
+            this.Name = "frmUpdateDog";
+            this.Text = "Update Dog";
+            this.grpDogInfo.ResumeLayout(false);
+            this.grpDogInfo.PerformLayout();
+            this.grpSex.ResumeLayout(false);
+            this.grpSex.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox grpDogInfo;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.DateTimePicker dtpChampionshipDate;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.DateTimePicker dtpDateBanned;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.CheckBox chkChampion;
+        private System.Windows.Forms.CheckBox chkBanned;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.CheckBox chkRetired;
+        private System.Windows.Forms.ComboBox cmbOwner;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox cmbBreed;
+        private System.Windows.Forms.DateTimePicker dtpDateOfRetirement;
+        private System.Windows.Forms.DateTimePicker dtpDateOfBirth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.GroupBox grpSex;
+        private System.Windows.Forms.RadioButton rdoFemale;
+        private System.Windows.Forms.RadioButton rdoMale;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnCancel;
     }
 }
